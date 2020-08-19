fdd75d361be8940149e620ed349786b1a0027f5b
iakovsidorov <ziforigin@gmail.com>
2020-08-19 15:19:53 +0300

    master branch initial state

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is Master branch in initial state");
-        System.out.println("My branch has some data");
-        System.out.println("John added something valuable");
     }
 }
--- src/main/java/MyClass.java
+++
@@ -1,3 +0,0 @@
-public class MyClass {
-    static final String MESSAGE = "This is my string in my class";
-}
